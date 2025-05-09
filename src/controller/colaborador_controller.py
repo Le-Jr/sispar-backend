c522b02ab9d62e3fd0be84f6f83cab7a0c5a06f6
Le-Jr <nascimentoleandrojr@gmail.com>
2025-05-09 18:03:37 -0300

    feat: add hash password function

--- src/controller/colaborador_controller.py
+++ src/controller/colaborador_controller.py
@@ -1,16 +1,18 @@
 from flask import Blueprint, jsonify, request
+from src.security.security import check_password, hash_password
 from src.model.colaborador_model import Employee
 from src.model import db
+
 
 bp_employee = Blueprint('colaborador', __name__, url_prefix='/colaborador')
 
 data = [
-    {'id': 1, 'nome': 'Samuel Silvério', 'cargo': 'Desenvolvedor Back-end', 'cracha': 'BE12310'},
-    {'id': 2, 'nome': 'Karynne Moreira', 'cargo': 'Desenvolvedora Front-end', 'cracha': 'FE21310'},
-    {'id': 3, 'nome': 'Joy Assis', 'cargo': 'Desenvolvedora Fullstack', 'cracha': 'FS12110'},
+    {'id': 1, 'name': 'Samuel Silvério', 'job': 'Desenvolvedor Back-end', 'badge': 'BE12310'},
+    {'id': 2, 'name': 'Karynne Moreira', 'job': 'Desenvolvedora Front-end', 'badge': 'FE21310'},
+    {'id': 3, 'name': 'Joy Assis', 'job': 'Desenvolvedora Fullstack', 'badge': 'FS12110'},
 ]
 
-@bp_employee.route('todos-colaboradores')
+@bp_employee.route('todos-colaboradores', methods = ["GET"])
 def get_data():
     employees = db.session.execute(
         db.select(Employee)
@@ -24,11 +26,12 @@ def get_data():
 @bp_employee.route('/criar', methods=["POST"])
 def create_employee():
     requisition_data = request.get_json()
+    password = hash_password(requisition_data['password'])
 
     new_employee = Employee(
         name=requisition_data['name'],
         email=requisition_data['email'],
-        password=requisition_data['password'],
+        password=password,
         job=requisition_data['job'],
         salary=requisition_data['salary']
     )
@@ -40,13 +43,37 @@ def create_employee():
         return jsonify({"Erro": "Insira todos os dados"}), 400
 
     # Verifica se já existe um crachá igual
-    for employee in data:
-        if employee['badge'] == requisition_data.get('badge'):
-            return jsonify({"Erro": "Crachá já existe"}), 400
+    # for employee in data:
+    #     if employee['badge'] == requisition_data.get('badge'):
+    #         return jsonify({"Erro": "Crachá já existe"}), 400
 
 
     data.append(new_employee)
     return jsonify({"message": "colaborador criado com sucesso"}), 201
+
+
+
+
+@bp_employee.route('login', methods = ["POST"])
+def login():
+    
+    requisition_data = request.get_json()
+    email = requisition_data['email']
+    password = requisition_data['password']
+    
+    if not email or not password:
+        return jsonify({"message": "preencha todos os campos de login"}), 400
+    
+    employee = db.session.execute(db.select(Employee).where(Employee.email == email)).scalar()
+    
+    if not employee:
+        return jsonify({"message": "Usuário não encontrado"}), 404
+    
+    employee = employee.to_dict()
+    
+    if employee.get('email') == email and check_password(password, employee.get('senha')):
+        return jsonify({"message":  "Login realizado co sucesso"}), 200
+
 
 
 
@@ -65,11 +92,11 @@ def update_employe_data(id_employee):
 
     # Verifica duplicação de crachá, se ele for alterado
     if 'badge' in requisition_data:
-        novo_cracha = requisition_data['badge']
+        novo_badge = requisition_data['badge']
         for employee in data:
-            if employee['badge'] == novo_cracha and employee['id'] != id_employee:
+            if employee['badge'] == novo_badge and employee['id'] != id_employee:
                 return jsonify({"Erro": "Crachá já existe"}), 400
-        employee_encontrado['badge'] = novo_cracha
+        employee_encontrado['badge'] = novo_badge
 
     if 'name' in requisition_data:
         employee_encontrado['name'] = requisition_data['name']
--- src/model/colaborador_model.py
+++ src/model/colaborador_model.py
@@ -2,12 +2,13 @@ from src.model import db
 from sqlalchemy.schema import Column
 from sqlalchemy.types import String, DECIMAL, Integer
 
+
 class Employee(db.Model):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(100))    
     email = Column(String(100))
-    password = Column(String(50))
+    password = Column(String(60))
     job = Column(String(100))
     salary = Column(DECIMAL(10, 2))
     
@@ -18,12 +19,20 @@ class Employee(db.Model):
         self.password = password
         self.job = job
         self.salary = salary
+        
+    def to_dict(self) -> dict:
+        return {
+            "email": self.email,
+            "password": self.password
+        } 
+        
 
 
     def all_data(self) -> dict:
         return {
             "id": self.id,
             "name": self.name,
+            "password": self.password,
             "job": self.job,
             "salary": self.salary
         }
