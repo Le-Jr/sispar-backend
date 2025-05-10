7fd174c552f0d6260048536d0d98ed993a67a883
Le-Jr <nascimentoleandrojr@gmail.com>
2025-05-10 14:18:21 -0300

    fix: size problem with password

--- src/security/security.py
+++ src/security/security.py
@@ -1,10 +1,8 @@
 from flask_bcrypt import bcrypt
 
-def hash_password(password):
-    salt = bcrypt.gensalt()
-    return bcrypt.hashpw(password.encode('utf-8'), salt)
+def hash_password(password: str) -> str:
+    hashed = bcrypt.generate_password_hash(password)  
+    return hashed.decode('utf-8')                    
 
-def check_password(password, password_hash):
-    return bcrypt.checkpw(password.encode('utf-8'), password_hash.encode('utf-8'))
-
-    
+def check_password(password: str, password_hash: str) -> bool:
+    return bcrypt.check_password_hash(password_hash, password)
