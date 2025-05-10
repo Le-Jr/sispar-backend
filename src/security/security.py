4bd328bf9f79936e597a6cd4d68a171a5d35c23f
Le-Jr <nascimentoleandrojr@gmail.com>
2025-05-10 14:28:25 -0300

    fix: bcrypt methods

--- src/security/security.py
+++ src/security/security.py
@@ -1,8 +1,12 @@
 from flask_bcrypt import bcrypt
 
 def hash_password(password: str) -> str:
-    hashed = bcrypt.generate_password_hash(password)  
-    return hashed.decode('utf-8')                    
+    salt   = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed.decode('utf-8')
 
 def check_password(password: str, password_hash: str) -> bool:
-    return bcrypt.check_password_hash(password_hash, password)
+    return bcrypt.checkpw(
+        password.encode('utf-8'),
+        password_hash.encode('utf-8')
+    )
